c3eccd2815e0369fe307e7448f5be76060717fde
AbdulazizAlharbi646 <113983169+AbdulazizAlharbi646@users.noreply.github.com>
2022-11-27 15:46:18 +0000

    pa

---
+++ IMM/Assets/Scripts/SceneResetter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneResetter : MonoBehaviour
+{
+    private void Update()
+    {
+        if (transform.position.y < -15)
+        {
+            GameManager.gameManager.menu.OpenMenuPanel();
+        }
+    }
+
+}
